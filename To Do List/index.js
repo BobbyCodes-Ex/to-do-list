900b798ef40291540426dc05462ba7802ba86115
Wesley Wood <wesleyowood@gmail.com>
2021-09-17 15:02:28 -0600

    Toggle Feature

--- To Do List/index.js
+++ To Do List/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  
     var getAndDisplayAllTasks = function () {
       $.ajax({
         type: 'GET',
@@ -7,7 +8,8 @@ $(document).ready(function(){
         success: function (response, textStatus) {
           $('#todo-list').empty();
           response.tasks.forEach(function (task) {
-            $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '>');
+            tasks = response.tasks
+            filterTasks();
           });
         },
         error: function (request, textStatus, errorMessage) {
@@ -15,7 +17,23 @@ $(document).ready(function(){
         }
       });
     }
-    
+    var tasks = [];
+    var filterTasks = function (element) {
+      
+      var status = $(element).data('type');
+      $('#todo-list').empty();
+      tasks.filter(function(task) {
+        if (status === 'all') {
+          return true;
+        } else if (status === 'completed') {
+          return task.completed;
+        } else if (status === 'active') {
+          return !task.completed;
+        }
+      }).forEach(function (task) {
+        $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '>');
+      });
+  }
     var createTask = function () {
       $.ajax({
         type: 'POST',
@@ -93,6 +111,11 @@ $(document).ready(function(){
       } else {
         markTaskActive($(this).data('id'));
       }
+    });
+
+    $('.filter').on('click', function (e) {
+      e.preventDefault();
+      filterTasks(this)
     });
   
     getAndDisplayAllTasks();
